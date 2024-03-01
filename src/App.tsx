055bcd4410f2a2d292ca63faf0545ae629c01d5b
Sergey Starchenko <pdxserg@gmail.com>
2024-02-29 18:37:41 -0800

    header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import styled from 'styled-components';
+import {Headr} from "./layout/headr/Headr";
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <Headr />
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/layout/headr/Headr.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo";
+import {Menu} from "../../components/menu/Menu";
+
+export const Headr = () => {
+	return (
+		<StyledHeader>
+			<Logo />
+			<Menu />
+
+		</StyledHeader>
+	);
+};
+
+const StyledHeader = styled.header`
+    background-color: #aae8e3;
+    display: flex;
+	justify-content: space-between;
+`
+
+
