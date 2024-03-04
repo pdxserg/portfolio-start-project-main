ca20684003f35f118ea349414e67b1cf3bc8d7a7
Sergey Starchenko <pdxserg@gmail.com>
2024-03-03 19:45:19 -0800

    creatWorks

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import {Headr} from "./layout/headr/Headr";
 import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills";
+import {Works} from "./layout/works/Works";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Headr />
             <Main />
             <Skills />
+            <Works />
         </div>
     );
 }
