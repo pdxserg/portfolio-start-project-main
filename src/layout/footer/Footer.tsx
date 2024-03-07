3e4b0850b4458e36a5d6cc8dd10540f876503964
Sergey Starchenko <pdxserg@gmail.com>
2024-03-06 22:05:36 -0800

    creatFooter

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import styled from "styled-components";
+import {SectionTitle} from "../../components/SectionTitle";
+import {Icon} from "../../components/icon/Icon";
+import {Menu} from "../../components/menu/Menu";
+import {FlexWrapper} from "../../components/FlexWrapper";
+
 
 export const Footer = () => {
 	return (
 		<StyledFooter>
-
+			<FlexWrapper direction={"column"} align={"center"}>
+				<SectionTitle>Olga</SectionTitle>
+				<SocialList>
+					<SocialItem>
+						<SocialIconLink>
+							<Icon iconId="insta"/>
+						</SocialIconLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialIconLink>
+							<Icon iconId="telegram"/>
+						</SocialIconLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialIconLink>
+							<Icon iconId="vk"/>
+						</SocialIconLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialIconLink>
+							<Icon iconId="linkedin"/>
+						</SocialIconLink>
+					</SocialItem>
+				</SocialList>
+				<Copyright>© 2023 Olga , All Rights Reserved.</Copyright>
+			</FlexWrapper>
 		</StyledFooter>
 	);
 };
 
-const StyledFooter = styled.section`
+const StyledFooter = styled.footer`
     background-color: #cbc3c3;
-    min-height: 50vh;
+    min-height: 20vh;
+
+`
+const SocialList = styled.ul`
+    display: flex;
+`
+
+const SocialItem = styled.li`
+    list-style-type: none;
+`
+const SocialIconLink = styled.a`
+
+
+`
+
+const Copyright = styled.small`
 `
