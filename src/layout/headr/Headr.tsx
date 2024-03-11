ce8bf4b8e131b686f9d2115971c2be7088fcba26
Sergey Starchenko <pdxserg@gmail.com>
2024-03-10 23:31:33 -0700

    Header2

--- src/layout/headr/Headr.tsx
+++ src/layout/headr/Headr.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./hederMenu/HederMenu";
 
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
@@ -13,7 +13,7 @@ export const Headr = () => {
 			<Container>
 				<FlexWrapper justify={"space-between"} align={"center"}>
 					<Logo />
-					<Menu menuItems={items} />
+					<HeaderMenu menuItems={items} />
 				</FlexWrapper>
 
 			</Container>
