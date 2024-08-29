9f669ff17400c88ec32be04a90ecc9fb7b2f0b20
SergeyStarchenko <pdxserg@gmail.com>
2024-08-28 22:26:49 -0700

    create filter tabsItems

--- src/layout/works/Works.tsx
+++ src/layout/works/Works.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SectionTitle} from "../../components/SectionTitle";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import photo from "../../assets/images/proj1.png"
@@ -8,14 +8,36 @@ import {Container} from "../../components/Container";
 import {Work} from "./work/Work";
 import {S} from "./Work_Styles"
 
-const items1 = ["All", "Landingpage", "React", "SPA"]
-
+// const tabsItem = ["All", "Landingpage", "React", "SPA"]
+type FiltereType = {}
+const tabsItem: Array<{
+	title: string,
+	status: 'all' | 'spa' | 'react' | 'landingpage'
+}> = [
+	{
+		title: "All",
+		status: "all"
+	},
+	{
+		title: "Landing page",
+		status: "landingpage"
+	},
+	{
+		title: "React",
+		status: "react"
+	},
+	{
+		title: "SPA",
+		status: "spa"
+	},
+]
 const worksDate = [
 
 	{
 		src: photo,
 		title: 'Social Network',
-		description: 'lorem10orem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliquaUt enim '
+		description: 'lorem10orem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliquaUt enim ',
+		type: "spa"
 	},
 	{
 		src: photo1,
@@ -23,23 +45,60 @@ const worksDate = [
 		description: 'lorem10orem ipsum dolor sit amet,' +
 			' consectetur adipisicing elit, sed do eiusmod ' +
 			'tempor incididunt ut labore et dolore magna aliqua' +
-			' Ut enim '
+			' Ut enim ',
+		type: "react"
+	},
+	{
+		src: photo1,
+		title: 'Timer',
+		description: 'lorem10orem ipsum dolor sit amet,' +
+			' consectetur adipisicing elit, sed do eiusmod ' +
+			'tempor incididunt ut labore et dolore magna aliqua' +
+			' Ut enim ',
+		type: "react"
+	},
+	{
+		src: photo1,
+		title: 'Landingpage',
+		description: 'lorem10orem ipsum dolor sit amet,' +
+			' consectetur adipisicing elit, sed do eiusmod ' +
+			'tempor incididunt ut labore et dolore magna aliqua' +
+			' Ut enim ',
+		type: "landingpage"
 	}
 ]
 export const Works: React.FC = () => {
+	const [curentFilterStatus, setCurentFilterStatus] = useState<'all' | 'spa' | 'react' | 'landingpage'>("all")
+
+	let filteredWorks = worksDate
+	if (curentFilterStatus === "landingpage") {
+		filteredWorks = worksDate.filter(el => el.type === "landingpage")
+	}
+	if (curentFilterStatus === "react") {
+		filteredWorks = worksDate.filter(el => el.type === "react")
+	}
+	if (curentFilterStatus === "spa") {
+		filteredWorks = worksDate.filter(el => el.type === "spa")
+	}
+const changeFilterStatus=(value: 'all' | 'spa' | 'react' | 'landingpage')=>{
+		setCurentFilterStatus(value)
+}
 	return (
 		<S.Works>
 			<Container>
 				<SectionTitle>My Work </SectionTitle>
-				<TabMenu menuItems={items1} />
+				<TabMenu tabsItems={tabsItem}
+				         changeFilterStatus={changeFilterStatus}
+				/>
 				<FlexWrapper justify={'space-between'} align={"flex-start"} wrap={"wrap"}>
 
-					{worksDate.map((w, index)=>{
+					{filteredWorks.map((w, index) => {
 						return <Work
-							key = {index}
-								src={w.src}
-							 	title={w.title}
-							 	description={w.description} />
+							key={index}
+
+							src={w.src}
+							title={w.title}
+							description={w.description}/>
 					})}
 				</FlexWrapper>
 			</Container>
--- src/layout/works/tabMenu/TabMenu.tsx
+++ src/layout/works/tabMenu/TabMenu.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import styled from "styled-components";
 import {LinkItem} from "../../../components/LinkItem";
 
-export const TabMenu = (props: { menuItems: Array<string> }) => {
+
+type TabMenuPropsType={
+	changeFilterStatus:(value:'all' | 'spa' | 'react' | 'landingpage')=>void
+	tabsItems: Array<{
+		title: string,
+		status: 'all' | 'spa' | 'react' | 'landingpage'}>
+}
+export const TabMenu = (props: TabMenuPropsType) => {
 	return (
 		<StyledMenu>
 			<ul>
-				{props.menuItems.map((item, index) => {
+				{props.tabsItems.map((item, index) => {
 						return <ListItem key={index}>
-							<LinkItem href="#">{item}</LinkItem>
+							<LinkItem as={'button'} onClick={()=>{props.changeFilterStatus(item.status)}}>
+								{item.title}
+							</LinkItem>
 						</ListItem>
 					}
 				)}
@@ -17,19 +26,19 @@ export const TabMenu = (props: { menuItems: Array<string> }) => {
 	);
 };
 const StyledMenu = styled.nav`
-    
+
     ul {
         display: flex;
         justify-content: space-between;
-	    max-width: 352px;
-	  width: 100%;
-	    border: 1px solid red;
+        max-width: 352px;
+        width: 100%;
+        border: 1px solid red;
         margin: 0 auto 40px;
     }
 
 `
 const ListItem = styled.li`
-	
-	`
+
+`
 
 
